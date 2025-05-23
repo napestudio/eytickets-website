28468a4f44029911c9010409b687ef705aa30bb8
cuerojr <cannibal5033@gmail.com>
2025-05-23 14:24:49 -0300

    wip: added svg detail con title price component

--- components/Prices.tsx
+++ components/Prices.tsx
@@ -4,12 +4,30 @@ import { cn } from "@/lib/utils";
 
 export default function Prices() {
   return (
-    <div>
+    <div className="mt-2">
       <div className="max-w-[80vw] md:max-w-[45vw] mx-auto space-y-10">
-        <div className="bg-black p-4 rounded-3xl text-center">
+        <div className="relative bg-black p-4 rounded-br-3xl rounded-bl-3xl text-center">
+          <svg
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            viewBox="0 0 520 520"
+            className="absolute top-0 -left-7 w-[30px] h-[30px] rotate-90"
+          >
+            <path d="M0,0h520C232.82,0,0,232.82,0,520V0Z" />
+          </svg>
           <h2 className="text-4xl uppercase text-primary font-black">
             Cobertura y precios
           </h2>
+          <svg
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            viewBox="0 0 520 520"
+            className="absolute top-0 -right-7 w-[30px] h-[30px]"
+          >
+            <path d="M0,0h520C232.82,0,0,232.82,0,520V0Z" />
+          </svg>
         </div>
         <p className="text-black font-semibold text-center">
           En EY Tickets comprás creditos por adelantado y usás uno por cada
